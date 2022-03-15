a00c218a7765a0224be6e17cf7a633a218e8f83d
Oleg Soll <sunskays@gmail.com>
2022-03-15 19:30:09 +0200

    ver.3.1 - Готовая база данных(добавлена валидация JOI)

--- src/api/cats/index.js
+++ src/api/cats/index.js
@@ -5,13 +5,16 @@ const express = require('express')
 const controllerCats = require('../../controllers/cats')
 const router = express.Router()
 
+// Валидация добавляется в самом финале, это отдельный файл и настройки
+const {validateCreateCat, validateUpdateCat, validateUpdateStatusCat} = require('../../validation/cats')
+
 // Делаем роутер - позволяет создавать цепочку, controllerCats - произвольное имя
 router
 .get('/', controllerCats.getAll)
 .get('/:id', controllerCats.getById)
-.post('/', controllerCats.create)
-.put('/:id', controllerCats.update)
-.patch('/:id/vaccinated', controllerCats.updateStatus)
+.post('/', validateCreateCat, controllerCats.create)
+.put('/:id', validateUpdateCat, controllerCats.update)
+.patch('/:id/vaccinated', validateUpdateStatusCat, controllerCats.updateStatus)
 .delete('/:id', controllerCats.remove)
 // .delete - в данном случае используется для понимания, на рпактике он "просто так" не используется
 
--- src/validation/cats.js
+++ src/validation/cats.js
@@ -1,3 +1,6 @@
+// мой импорт:
+const { HttpCode } = require("../helpers/constants")
+
 // Взято из библиотеки https://joi.dev/api/?v=17.6.0
 
 const Joi = require("joi")
@@ -18,14 +21,11 @@ const schemaCreatedCat = Joi.object({
 
   // password: Joi.string()
   //     .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-
   // repeat_password: Joi.ref('password'),
-
   // access_token: [
   //     Joi.string(),
   //     Joi.number()
   // ],
-
   // email: Joi.string()
   //     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
 })
@@ -45,14 +45,36 @@ const schemaUpdateCat = Joi.object({
   isVaccinated: Joi.boolean().optional(),
 })
 
-const schemaUpdateCat = Joi.object({
-    name: Joi.string().alphanum().min(2).max(30)
-      // Указали опционально
-      .optional(),
-  
-    age: Joi.number().integer().min(1).max(45)
-      // Указали опционально
-      .optional(),
-  
-    isVaccinated: Joi.boolean().optional(),
+const schemaUpdateStatusCat = Joi.object({
+    // Указали что обязательно
+    isVaccinated: Joi.boolean().required(),
   })
+
+// Описание смотри в доках библиотеки:
+const validate = (schema, body, next) => {
+    const {error} = schema.validate(body)
+    if (error) {
+        const [{message}] = error.details
+
+        return next({
+            status: HttpCode.BAD_REQUEST,
+            // в message будет показываться информация для пользователя в случае ошибки
+            message: `Данное поле обязательное: ${message.replace(/"/g, '')}`,
+            data: "Bad Request",
+          })
+    }
+    next()
+}
+
+// Взято от преподователя(специальный импорт):
+module.exports.validateCreateCat = (req, res, next) => {
+    return validate(schemaCreatedCat, req.body, next)
+}
+
+module.exports.validateUpdateCat = (req, res, next) => {
+    return validate(schemaUpdateCat, req.body, next)
+}
+
+module.exports.validateUpdateStatusCat = (req, res, next) => {
+    return validate(schemaUpdateStatusCat, req.body, next)
+}
